94f27f2ff9584149354591c8434fa19508c4dc0b
Bruno Bitencourt <bruno.bitencourt231@gmail.com>
2026-09-21 10:48:02 -0300

    dados e script - Etapa 2

--- script_aula_14_extra.R
+++ script_aula_14_extra.R
@@ -141,4 +141,6 @@ BACO_AULA14_RJ <- bind_rows(resumo_uf, resumo_mun)
 
 # Tarefa 5: Exportar o banco de dados BANCO_AULA14_RJ com o nome BANCO_AULA14_RJ.csv
 
+write.csv2(BACO_AULA14_RJ, "BANCO_AULA14_RJ.csv", row.names = FALSE)
+
 # Ao terminar a Tarefa 5 commit com a mensagem "dados e script - Etapa 2" e envie para o repositório Aula_14_Extra
